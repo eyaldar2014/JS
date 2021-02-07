fba9f54f156077e8da1841dfb74888386076305d
Eyal Dar <eyaldar2014@gmail.com>
2021-02-07 11:48:12 +0200

    1.1

---
+++ 1.1_basics/index.js
@@ -0,0 +1,25 @@
+
+
+const string = 'asaaa';
+const int = 111;
+
+let books = {
+  first: 'One flew over the cuccuns nest',
+  second: 'Badolina'
+}
+
+let Eyal = {
+  name: "Eyal",
+  age: 28,
+  hobby: ["music", books],
+  handsome: true,
+}
+
+let nul = null;
+
+const und = undefined;
+
+
+let symbol = Symbol('$');
+
+console.log(symbol);
