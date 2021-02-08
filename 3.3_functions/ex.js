0ba64468697a813b85f49dcbf4d0fa596ce7b453
Eyal Dar <eyaldar2014@gmail.com>
2021-02-08 10:07:34 +0200

    few pdf

--- 3.3_functions/ex.js
+++ 3.3_functions/ex.js
@@ -20,16 +20,16 @@ console.log("'I' population is around (" +me+ ")% of the world")
 
 // expression:
 
-let calculation = function percentageOfWorld2(n){
+let percentageOfWorld2 = (n) => {
   let result = n / worldPopulation * 100 ;
   return result
 }
 
-let Africa2 = calculation(1000)
+let Africa2 = percentageOfWorld2(1000)
 console.log("Africa's population is around " +Africa2+ "% of the world")
 
-let Australia2 = calculation(22)
+let Australia2 = percentageOfWorld2(22)
 console.log("Australia's population is around " +Australia2+ "% of the world")
 
-let me2 = calculation(0.000001)
+let me2 = percentageOfWorld2(0.000001)
 console.log("'I' population is around (" +me2+ ")% of the world")
