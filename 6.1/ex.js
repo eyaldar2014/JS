5680d0fc045d7c1c7d8fbc6586c9992c01859177
Eyal Dar <eyaldar2014@gmail.com>
2021-02-10 09:32:53 +0200

    6.1

--- 6.1/ex.js
+++ 6.1/ex.js
@@ -1,9 +1,8 @@
-// 3. Create function “arraySum” , the function return the sum of all elements in array.
-// 4. Create function “arrayMulti” , the function return the multiplication of all the elements in array.
+
 
 // 1. Print with for loop all the number.
 
-const arr = [1,7,3,0,-5,7,3,9];
+const arr = [1,7,3,-5,7,3,9, 1 ];
 
 for (i=0 ; i < arr.length ; i++ ){
   console.log(arr[i])
@@ -30,5 +29,24 @@ arrayLength(arr)
 // 3. Create function “arraySum” , the function return the sum of all elements in array.
 
 const arraySum = (array) =>{
-  
+  let sum = 0
+  for (i=0 ; i < array.length ; i++ ){
+    sum = sum + array[i]
+  }
+  console.log(sum)
 }
+
+arraySum(arr)
+
+
+// 4. Create function “arrayMulti” , the function return the multiplication of all the elements in array.
+
+const arrayMulti = (array) =>{
+  let multi = 1
+  for (i=0 ; i < array.length ; i++ ){
+    multi = multi * array[i]
+  }
+  console.log(multi)
+}
+
+arrayMulti(arr)
