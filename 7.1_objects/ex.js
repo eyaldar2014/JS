332d138462f760ef4a1fe72ceff13c44f5c10ddf
Eyal Dar <eyaldar2014@gmail.com>
2021-02-10 13:29:57 +0200

    organize 6 & 7 folders

---
+++ 7.1_objects/ex.js
@@ -0,0 +1,4 @@
+
+// 1. Create an object that represents a book. It should have 4-5 properties that make sense for a book to have.
+// 2. Then write a function that has one argument, a book, that will return the following description from your book object:
+// `The book <book name> was written by <author name> in the year <publishing yeary
