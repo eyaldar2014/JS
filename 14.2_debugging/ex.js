d1f6726954e328879221bfa8d381e405051fa1a9
Eyal Dar <eyaldar2014@gmail.com>
2021-02-16 14:58:47 +0200

    14.3

--- 14.2_debugging/ex.js
+++ 14.2_debugging/ex.js
@@ -13,7 +13,7 @@ function findSmallest(a, b, c) {
   }
 }
 
-findSmallest(52, 66, 2);
+sfindSmallest(52, 66, 2);
 
 
 
---
+++ 14.3_debugging/ex.js
@@ -0,0 +1,21 @@
+
+
+function getSumOfEven(arr) {
+  return arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
+}
+
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+
+// What is wrong with this code?
+
+// 1. First find the line that contains the problem. Write it down. 
+// 5
+
+// 2. Which debug method did you use to find the bug?
+// debugger and discover when the issue happens
+
+// 3. Explain the bug in your own words.
+// index of 10  doesn't exist
+
+// 4. Fix the code and submit it all.
