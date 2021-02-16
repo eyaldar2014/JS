285a0ccff10896ee751bd5c4bd8df7a3a104038b
Eyal Dar <eyaldar2014@gmail.com>
2021-02-16 15:15:54 +0200

    14.4

---
+++ 14.4_debugging/ex.js
@@ -0,0 +1,25 @@
+function calcAverage (arr){
+  debugger
+  var sum = 0;
+  for(var i = 0 ; i < arr.length; i ++){ 
+    sum += arr[ i ];
+  }
+
+  return sum ;
+  }
+
+  calcAverage ([ 85 , 90 , 92 ]);
+
+
+//   What is wrong with this code?
+// 1. First find the line that contains the problem. Write it down.
+// 3
+
+// 2. Which debug method did you use to find the bug?
+// debugger tool in vs code
+
+// 3. Explain the bug in your own words.
+// because sum is undefined it cant use as a counter
+
+// 4. Fix the code and submit it all.
+
