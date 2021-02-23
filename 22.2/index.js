8ee8b46e78885c7da5e6367b14fd0c7fb26a9601
Eyal Dar <eyaldar2014@gmail.com>
2021-02-23 22:04:52 +0200

    22.2

--- 22.2/index.js
+++ 22.2/index.js
@@ -1,1 +1,21 @@
-console.log('a')
+// console.log('a')
+
+// Create a webpage that has a checkbox. If the checkbox is checked, make a photo appear. If it is not, make it disappear.
+
+
+let checkbox = document.querySelector('#checkbox')
+
+let photo = document.querySelector('#photo')
+
+
+document.addEventListener('click', (e)=>{
+
+  if (checkbox.checked === true) {
+    photo.style.display = 'block'
+  }
+  
+  if (checkbox.checked === false) {
+    photo.style.display = 'none'
+  }
+
+})
